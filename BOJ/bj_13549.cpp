ce6bc84d84c2cedcf5e7eeb1aecf8007a9a78da3
hojooneum <bldolphin96@gmail.com>
2022-07-21 23:26:04 +0900

    [BOJ] update bj_13549

--- BOJ/bj_13549.cpp
+++ BOJ/bj_13549.cpp
@@ -1,37 +1,92 @@
 #include <stdio.h>
 #include <queue>
 #include <utility>
-#define MAXSIZE 100001
+
 using namespace std;
+struct Item
+{
+    int n;
+    int g;
+    int estimation;
+
+    bool operator< (const Item& item)const {
+        if (estimation == item.estimation) {
+            return g > item.g;
+        }
+        else {
+            return estimation > item.estimation;
+        }
+    }
+};
 
 int calculateMinTime(const int N, const int K);
+int estimateMinTime(const int n, const int g, const int K);
+struct Item makeItem(int n, int g, int estimation);
+
 int main() {
     int N, K;
     scanf("%d %d", &N, &K);
 
     int minTime = calculateMinTime(N,K);
     printf("%d\n", minTime);
+    return 0;
 }
 
 int calculateMinTime(const int N, const int K) {
     if (N > K) {
         return N - K;
     }
-    int minTime;
-    pair<int,int> n;
-    queue<pair<int,int>> q;
-    q.push({N,0});
+    int minTime = INT_MAX;
+    Item currentNumber;
+    priority_queue<Item> q;
+    q.push({0,N});
     while (q.empty() == false) {
-        n = q.front();
+        currentNumber = q.top();
         q.pop();
-        if (n.first == K) {
-            if (n.second < minTime) {
-                minTime = n.second;
+        if (currentNumber.n == K) {
+            if (currentNumber.g < minTime) {
+                minTime = currentNumber.g;
             }
         }
         else {
-            int h = std::abs(K - n.first);
+            int estimation1 = estimateMinTime(currentNumber.n + 1, currentNumber.g + 1, K);
+            int estimation2 = estimateMinTime(currentNumber.n - 1, currentNumber.g + 1, K);
+            int estimation3 = estimateMinTime(currentNumber.n * 2, currentNumber.g , K);
+            if (estimation1 < minTime) {
+                q.push(makeItem(currentNumber.n + 1, currentNumber.g + 1, estimation1));
+            }
+            if (estimation2 < minTime) {
+                q.push(makeItem(currentNumber.n - 1, currentNumber.g + 1, estimation2));
+            }
+            if (estimation3 < minTime) {
+                q.push(makeItem(currentNumber.n * 2, currentNumber.g, estimation3));
+            }
         }
     }
+    return minTime;
+}
 
+struct Item makeItem(int n, int g, int estimation) {
+    struct Item item = {n,g,estimation};
+    return item;
+}
+
+int estimateMinTime(const int n, const int g, const int K) {
+    int estimation = g;
+    int h = 0;
+    int N = n;
+    int doubles = 0;
+    if (n > K) {
+        h += n - K;
+    }
+    else {
+        while (N < K)
+        {
+            N = N * 2;
+            doubles += 1;
+        }
+        h += doubles;
+    }
+    estimation += h;
+    return estimation;
 }
