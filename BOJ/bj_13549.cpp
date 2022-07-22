8b312c17c451f241c0ba3dbb4bae737b7937b189
hojooneum <bldolphin96@gmail.com>
2022-07-22 23:57:49 +0900

    [BOJ] update bj_13549

--- BOJ/bj_13549.cpp
+++ BOJ/bj_13549.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <queue>
 #include <utility>
-
+#include <array>
+#define MAXSIZE 100001
 using namespace std;
 struct Item
 {
@@ -9,7 +10,7 @@ struct Item
     int g;
     int estimation;
 
-    bool operator< (const Item& item)const {
+    bool operator< (const Item & item)const {
         if (estimation == item.estimation) {
             return g > item.g;
         }
@@ -33,13 +34,20 @@ int main() {
 }
 
 int calculateMinTime(const int N, const int K) {
+    array<int, MAXSIZE*2> positions;
+    array<int, MAXSIZE*2> calculated;
+    positions.fill(MAXSIZE);
+    calculated.fill(false);
     if (N > K) {
         return N - K;
     }
-    int minTime = INT_MAX;
+    int minTime = MAXSIZE;
     Item currentNumber;
     priority_queue<Item> q;
-    q.push({0,N});
+    int initialEstimation = estimateMinTime(N,0,K);
+    q.push(makeItem(N,0,initialEstimation));
+    positions[N] = 0;
+    calculated[N] = true;
     while (q.empty() == false) {
         currentNumber = q.top();
         q.pop();
@@ -48,18 +56,22 @@ int calculateMinTime(const int N, const int K) {
                 minTime = currentNumber.g;
             }
         }
+        if (positions[currentNumber.n] < currentNumber.g) {
+            continue;
+        }
         else {
+            positions[currentNumber.n] = currentNumber.g;
             int estimation1 = estimateMinTime(currentNumber.n + 1, currentNumber.g + 1, K);
             int estimation2 = estimateMinTime(currentNumber.n - 1, currentNumber.g + 1, K);
             int estimation3 = estimateMinTime(currentNumber.n * 2, currentNumber.g , K);
-            if (estimation1 < minTime) {
+            if (estimation1 < minTime && positions[currentNumber.n + 1] > estimation1 && calculated[currentNumber.n + 1] == false) {
                 q.push(makeItem(currentNumber.n + 1, currentNumber.g + 1, estimation1));
             }
-            if (estimation2 < minTime) {
+            if (estimation2 < minTime && positions[currentNumber.n - 1] > estimation2 && calculated[currentNumber.n - 1] == false) {
                 q.push(makeItem(currentNumber.n - 1, currentNumber.g + 1, estimation2));
             }
-            if (estimation3 < minTime) {
-                q.push(makeItem(currentNumber.n * 2, currentNumber.g, estimation3));
+            if (estimation3 < minTime && positions[currentNumber.n * 2] > estimation3 && calculated[currentNumber.n * 2] == false) {
+                q.push(makeItem(currentNumber.n * 2, currentNumber.g + 1, estimation3));
             }
         }
     }
@@ -76,8 +88,12 @@ int estimateMinTime(const int n, const int g, const int K) {
     int h = 0;
     int N = n;
     int doubles = 0;
+    
     if (n > K) {
         h += n - K;
+    }
+    else if (n == 0) {
+        h = K - n;
     }
     else {
         while (N < K)
