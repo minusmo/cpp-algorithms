1d9d86732a4fab95f8ded3aec0082a89a5e339f2
hojooneum <bldolphin96@gmail.com>
2022-08-01 13:41:23 +0900

    [BOJ] solve bj_2096_DP

--- BOJ/bj_2096_DP.cpp
+++ BOJ/bj_2096_DP.cpp
@@ -3,34 +3,70 @@
 #define MAXSIZE 100000
 using namespace std;
 
-int calculateMinVal(int numbers[MAXSIZE][3], const int N);
-int calculateMaxVal(int numbers[MAXSIZE][3], const int N);
+void calculateMinMaxVal(int numbers[MAXSIZE][3], const int N, int* minVal, int* maxVal);
+int getMin(const int a, const int b);
+int getMax(const int a, const int b);
 
 int main() {
-    int minVal = 900001; 
-    int maxVal = 0;
+    int minVal; 
+    int maxVal;
     int N;
     int numbers[MAXSIZE][3];
     cin >> N;
     for (int i=0;i<N;i++) {
-        cin >> numbers[i][0] >> numbers[i][1] >> numbers[i][2];
+        for (int j=0;j<3;j++) {
+            cin >> numbers[i][j];
+        }
     }
-    minVal = calculateMinVal(numbers, N);
-    maxVal = calculateMaxVal(numbers, N);
+    calculateMinMaxVal(numbers, N, &minVal, &maxVal);
 
     cout << maxVal << ' ' << minVal << endl;
     return 0;
 }
 
-int calculateMinVal(int numbers[MAXSIZE][3], const int N) {
-    int minMaxVals[6] = {numbers[0][0],numbers[0][1],numbers[0][2],numbers[0][0],numbers[0][1],numbers[0][2]};
+void calculateMinMaxVal(int numbers[MAXSIZE][3], const int N, int* minVal, int* maxVal) {
+    int min0, min1, min2, max0, max1, max2;
+    int maxVals[3] = {numbers[0][0],numbers[0][1],numbers[0][2]};
+    int minVals[3] = {numbers[0][0],numbers[0][1],numbers[0][2]};
     for (int i=1;i<N;i++) {
-        minMaxVals[0] = numbers[i][0] + std::min(minMaxVals[1], minMaxVals[2]);
-        minMaxVals[1] = numbers[i][1] + std::min(minMaxVals[0], minMaxVals[2]);
-        minMaxVals[2] = numbers[i][2] + std::min(minMaxVals[0], minMaxVals[1]);
-        minMaxVals[3] = numbers[i][0] + std::max(minMaxVals[4], minMaxVals[5]);
-        minMaxVals[4] = numbers[i][1] + std::max(minMaxVals[4], minMaxVals[5]);
-        minMaxVals[5] = numbers[i][2] + std::max(minMaxVals[3], minMaxVals[4]);
+        min0 = minVals[0];
+        min1 = minVals[1];
+        min2 = minVals[2];
+        max0 = maxVals[0];
+        max1 = maxVals[1];
+        max2 = maxVals[2];
+        minVals[0] = numbers[i][0] + getMin(min0, min1);
+        minVals[1] = numbers[i][1] + getMin(getMin(min0, min1), min2);
+        minVals[2] = numbers[i][2] + getMin(min1, min2);
+        maxVals[0] = numbers[i][0] + getMax(max0, max1);
+        maxVals[1] = numbers[i][1] + getMax(getMax(max0, max1), max2);
+        maxVals[2] = numbers[i][2] + getMax(max1, max2);
     }
 
+    *minVal = getMin(getMin(minVals[0], minVals[1]), minVals[2]);
+    *maxVal = getMax(getMax(maxVals[0], maxVals[1]), maxVals[2]);
+}
+
+int getMin(const int a, const int b) {
+    if (a > b) {
+        return b;
+    }
+    else if (a < b) {
+        return a;
+    }
+    else {
+        return a;
+    }
+}
+
+int getMax(const int a, const int b) {
+    if (a < b) {
+        return b;
+    }
+    else if (a > b) {
+        return a;
+    }
+    else {
+        return a;
+    }
 }
