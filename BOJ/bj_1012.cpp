95b0fc6b1aba16dcb40ca6f77ae5146699ddad91
hojooneum <bldolphin96@gmail.com>
2022-09-01 23:54:55 +0900

    update bj 1012

--- BOJ/bj_1012.cpp
+++ BOJ/bj_1012.cpp
@@ -6,53 +6,84 @@
 #define MAXPOS 2500
 using namespace std;
 
-int calculateWorms(const int M, const int N, const int K, vector<pair<int, int> > spots,int map[MAXSIZE][MAXSIZE]);
+int calculateWorms(const int M, const int N, const int K,int map[MAXSIZE][MAXSIZE]);
 void bfs(int visited[MAXSIZE][MAXSIZE], const int r, const int c, const int M, const int N, int map[MAXSIZE][MAXSIZE]);
 int main() 
 {
     int T;
+    scanf("%d", &T);
+    int answers[T];
     for (int t=0;t<T;t++)
     {
-        int M, N, K, r, c;
-        cin >> T;
-        cin >> M >> N >> K;
         int map[MAXSIZE][MAXSIZE];
-        vector<pair<int,int> > spots;
-        for (int i=0;i<K;i++)
+        int M, N, K, r, c;
+        scanf("%d %d %d", &M, &N,&K);
+        for (r=0;r<N;r++)
         {
-            cin >> c >> r;
-            map[r][c] = 1;
-            spots.push_back({r,c});
+            for (c=0;c<M;c++)
+            {
+                map[r][c] = 0;
+            }
         }
-        int worms = calculateWorms(M,N,K,spots,map);
-        cout << worms << endl;
+        for (int k=0;k<K;k++)
+        {
+            scanf("%d %d", &c, &r);
+            map[r][c] = 1;
+        }
+        int worms = calculateWorms(M,N,K,map);
+        answers[t] = worms;
+    }
+    for (int t=0;t<T;t++)
+    {
+        printf("%d\n", answers[t]);
     }
     return 0;
 }
 
-int calculateWorms(const int M, const int N, const int K, vector<pair<int, int> > spots, int map[MAXSIZE][MAXSIZE])
+int calculateWorms(const int M, const int N, const int K, int map[MAXSIZE][MAXSIZE])
 {
     int r, c;
     int worms = 0;
-    int visited[MAXSIZE][MAXSIZE];
-    for (r=0;r<M;r++)
+    int visited[N][M];
+    for (r=0;r<N;r++)
     {
-        for (c=0;c<N;c++)
+        for (c=0;c<M;c++)
         {
             visited[r][c] = 0;
         }
     }
-    for (auto spot : spots)
+    for (r=0;r<N;r++)
     {
-        r = spot.first;
-        c = spot.second;
-        if (visited[r][c] == 1)
+        for (c=0;c<M;c++)
         {
-            continue;
-        }
-        else {
-            bfs(visited,r,c,M,N,map);
-            worms += 1;
+            if (map[r][c] == 1 && visited[r][c] == 0)
+            {
+                vector<pair<int,int> > moves = {{-1,0},{0,1},{1,0},{0,-1}};
+                queue<pair<int,int> > q;
+                q.push({r,c});
+                while (!q.empty())
+                {
+                    auto spot = q.front();
+                    q.pop();
+                    visited[spot.first][spot.second] = 1;
+                    for (auto move : moves)
+                    {
+                        int newr = spot.first + move.first;
+                        int newc = spot.second + move.second;
+                        if (newr >= N || newc >= M || newr < 0 || newc < 0)
+                        {
+                            continue;
+                        }
+                        else if (map[newr][newc] == 1 && visited[newr][newc] == 0)
+                        {
+                            q.push({newr,newc});
+                        }
+                    }
+                        
+                }
+                worms += 1;
+            }
+            
         }
     }
     return worms;
@@ -60,36 +91,28 @@ int calculateWorms(const int M, const int N, const int K, vector<pair<int, int> 
 
 void bfs(int visited[MAXSIZE][MAXSIZE], const int r, const int c, const int M, const int N, int map[MAXSIZE][MAXSIZE])
 {
-    vector<pair<int,int> > moves = {{0,-1},{1,0},{0,1},{-1,0}};
+    vector<pair<int,int> > moves = {{-1,0},{0,1},{1,0},{0,-1}};
     queue<pair<int,int> > q;
     q.push({r,c});
     while (!q.empty())
     {
         auto spot = q.front();
         q.pop();
-        if (visited[spot.first][spot.second] == 1)
+        visited[spot.first][spot.second] = 1;
+        for (auto move : moves)
         {
-            continue;
-        }
-        else
-        {
-            visited[r][c] = 1;
-            for (auto move : moves)
+            int newr = spot.first + move.first;
+            int newc = spot.second + move.second;
+            if (newr >= N || newc >= M || newr < 0 || newc < 0)
             {
-                int newr = spot.first + move.first;
-                int newc = spot.second + move.second;
-                if (newr >= M || newc >= N || newr < 0 || newc < 0)
-                {
-                    continue;
-                }
-                else if (map[newr][newc] == 1 && visited[newr][newc] == 0)
-                {
-                    visited[newr][newc] = 1;
-                    q.push({newr,newc});
-                }
+                continue;
             }
-            
+            else if (map[newr][newc] == 1 && visited[newr][newc] == 0)
+            {
+                q.push({newr,newc});
+            }
         }
+            
     }
     
 }
