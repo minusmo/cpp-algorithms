3090da276ee83b76e5ffcea7b53831b1a8cd6a8a
hojooneum <bldolphin96@gmail.com>
2022-09-07 21:31:42 +0900

    solve bj 1260

--- BOJ/bj_1260.cpp
+++ BOJ/bj_1260.cpp
@@ -4,8 +4,8 @@
 #define MAXEDGES 10000
 using namespace std;
 
-void dfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1][MAXNODES+1], int currentNode);
-void bfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1][MAXNODES+1], int currentNode);
+void dfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1], int currentNode);
+void bfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1], int currentNode);
 
 int main() 
 {
@@ -24,62 +24,37 @@ int main()
     {
         cin >> depart >> arrival;
         adjacencyMatrix[depart][arrival] = 1;
+        adjacencyMatrix[arrival][depart] = 1;
     }
 
-    bool isVisitedDfs[MAXNODES+1][MAXNODES+1];
+    bool isVisitedDfs[MAXNODES+1];
+    bool isVisitedBfs[MAXNODES+1];
     for (int i=1;i<MAXNODES+1;i++)
     {
-        for (int j=1;j<MAXNODES+1;j++)
-        {
-            if (j == V)
-            {
-                isVisitedDfs[i][V] = true;
-            }
-            else
-            {
-                isVisitedDfs[i][j] = false;
-            }
-        }
+            isVisitedDfs[i] = false;
+            isVisitedBfs[i] = false;
     }
     dfs(adjacencyMatrix, isVisitedDfs, V);
     printf("\n");
 
-    bool isVisitedBfs[MAXNODES+1][MAXNODES+1];
-    for (int i=1;i<MAXNODES+1;i++)
-    {
-        for (int j=1;j<MAXNODES+1;j++)
-        {
-            if (j == V)
-            {
-                isVisitedBfs[i][V] = true;
-            }
-            else
-            {
-                isVisitedBfs[i][j] = false;
-            }
-        }
-    }
     bfs(adjacencyMatrix, isVisitedBfs, V);
     printf("\n");
     return 0;
 }
 
-void dfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1][MAXNODES+1], int currentNode) {
+void dfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1], int currentNode) {
     printf("%d ", currentNode);
+    isVisited[currentNode] = true;
     for (int i=1;i<MAXNODES+1;i++)
     {
-        if (adjacencyMatrix[currentNode][i] == 1 && isVisited[currentNode][i] == false)
+        if (adjacencyMatrix[currentNode][i] == 1 && isVisited[i] == false)
         {
-            for (int j=1;j<MAXNODES+1;j++)
-            {
-                isVisited[j][i] = true;
-            }
             dfs(adjacencyMatrix, isVisited, i);
         }   
     }
 }
 
-void bfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1][MAXNODES+1], int currentNode)
+void bfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1], int currentNode)
 {
     queue<int> q;
     q.push(currentNode);
@@ -87,18 +62,19 @@ void bfs(int adjacencyMatrix[MAXNODES+1][MAXNODES+1], bool isVisited[MAXNODES+1]
     {
         auto visitingNode = q.front();
         q.pop();
-        printf("%d ", visitingNode);
-        for (int j=1;j<MAXNODES+1;j++)
+        if (isVisited[visitingNode] == false)
         {
-            isVisited[j][visitingNode] = true;
-        }
-        for (int i=1;i<MAXNODES+1;i++)
-        {
-            if (adjacencyMatrix[visitingNode][i] == 1 && isVisited[visitingNode][i] == false)
+            printf("%d ", visitingNode);
+            isVisited[visitingNode] = true;
+            for (int i=1;i<MAXNODES+1;i++)
             {
-                q.push(i); 
+                if (adjacencyMatrix[visitingNode][i] == 1 && isVisited[i] == false)
+                {
+                    q.push(i); 
+                }
             }
-        }
+        } 
+        
     }
     
 }
