37be4aa009b4d319a87d4bd28264b0fdae5dfa35
hojooneum <bldolphin96@gmail.com>
2022-09-11 01:13:01 +0900

    Update bj 11724 algorithm to use adjacency list

--- BOJ/bj_11724.cpp
+++ BOJ/bj_11724.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 #define MAXNODES 1000
 #define MAXEDGES 500000
 using namespace std;
 
-int getConnectedComponents(int adjMat[MAXNODES+1][MAXNODES+1], int N, int M);
+int getConnectedComponents(vector<int> adjlist[MAXNODES+1], int N, int M);
 int main() {
     int N,M,a,b;
-    int adjMat[MAXNODES+1][MAXNODES+1];
     cin >> N >> M;
-    for (int i=0;i<N+1;i++)
-    {
-        for (int j=0;j<N+1;j++)
-        {
-            adjMat[i][j] = 0;
-        }
-    }
+    vector<int> adjlist[MAXNODES+1];
     for (int i=0;i<M;i++)
     {
         cin >> a >> b;
-        adjMat[a][b] = 1;
-        adjMat[b][a] = 1;
+        adjlist[a].push_back(b);
+        adjlist[b].push_back(a);
     }
-    int connectedComponents = getConnectedComponents(adjMat,N,M);
+    int connectedComponents = getConnectedComponents(adjlist,N,M);
     cout << connectedComponents << endl;
     return 0;
 }
 
-int getConnectedComponents(int adjMat[MAXNODES+1][MAXNODES+1], int N, int M)
+int getConnectedComponents(vector<int> adjlist[MAXNODES+1], int N, int M)
 {
     bool isVisited[MAXNODES+1];
+    int connectedComponents = 0;
     for (int i=1;i<N+1;i++)
     {
         isVisited[i] = false;
@@ -43,19 +38,20 @@ int getConnectedComponents(int adjMat[MAXNODES+1][MAXNODES+1], int N, int M)
             q.push(j);
             while (!q.empty())
             {
-                auto node = q.front();
+                int node = q.front();
                 q.pop();
                 isVisited[node] = true;
-                for (int i = 1; i < N+1; i++)
+                for (auto adjnode : adjlist[node])
                 {
-                    if (adjMat[node][i] == 1 && isVisited[i] == false)
+                    if (isVisited[adjnode] == false)
                     {
-                        q.push(i);
+                        q.push(adjnode);
                     }
                 }
                 
             }
-            
+            connectedComponents += 1;
         }
     }
+    return connectedComponents;
 }
