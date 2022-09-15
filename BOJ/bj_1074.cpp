418aaa6ba0e1b7c17e23cfaaff88798381725c11
hojooneum <bldolphin96@gmail.com>
2022-09-15 23:43:53 +0900

    Solve bj 1074

--- BOJ/bj_1074.cpp
+++ BOJ/bj_1074.cpp
@@ -3,43 +3,54 @@
 #define MAX 15
 using namespace std;
 
-void visitZ(int N,int stR, int stC, const int R, const int C, int* visits, int* isFound);
+void visitZ(int N,int stR, int stC, const int R, const int C, long* visits, int* isFound);
 int main() 
 {
     int N,R,C;
-    int visits = 0;
+    long visits = 0;
     int isFound = 0;
     scanf("%d %d %d", &N, &R, &C);
-    visitZ(N,(int)pow(2,N),(int)pow(2,N),R,C,&visits, &isFound);
+    int width = (int)pow(2,N)-1;
+    visitZ(N,width,width,R,C,&visits, &isFound);
     return 0;
 }
 
-void visitZ(int N,int stR, int stC, const int R, const int C, int* visits, int* isFound)
+void visitZ(int N,int stR, int stC, const int R, const int C, long* visits, int* isFound)
 {
     if (*isFound == 1)
     {
         return;
     }
-    else if (N > 0)
+    else if (N > 1)
     {
-        if (R >= stR || C >= stC)
+        if (R > stR || C > stC)
         {
-            *visits += N * N;
+            int width = (int)pow(2,N);
+            *visits += width * width;
             return;
         }
-        visitZ(N-1, stR/2, stC/2, R,C,visits, isFound);
-        visitZ(N-1, stR/2, stC, R,C,visits, isFound);
-        visitZ(N-1, stR,stC/2, R,C,visits, isFound);
+        int halfWidth = (int)pow(2,N-1);
+        visitZ(N-1, stR - halfWidth, stC - halfWidth, R,C,visits, isFound);
+        visitZ(N-1, stR - halfWidth, stC, R,C,visits, isFound);
+        visitZ(N-1, stR,stC - halfWidth, R,C,visits, isFound);
         visitZ(N-1, stR, stC, R,C,visits, isFound);
     }
     else
     {
-        if (stR-1 == R && stC-1 == C)
+        for (int r = stR-1; r < stR+1; r++)
         {
-            printf("%d\n", *visits);
-            *isFound = 1;
-            return;
+            for (int c = stC-1; c < stC+1; c++)
+            {
+                if (r == R && c == C)
+                {
+                    printf("%ld\n", *visits);
+                    *isFound = 1;
+                    return;
+                }
+                *visits += 1;
+            }
+            
         }
-        *visits += 1;
+        
     }
 }
